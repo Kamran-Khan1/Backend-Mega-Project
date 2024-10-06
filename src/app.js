42624edc021b2575031c57228dadebeb13533c7e
Kamran Khan <kamran263653@gmail.com>
2024-10-06 16:36:08 +0600

    All setup completed

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.json({ limit: "18kb" }));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "18kb" }));
 app.use(cookieParser());
+
+//routes
+
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoutes);
+
 export { app };
